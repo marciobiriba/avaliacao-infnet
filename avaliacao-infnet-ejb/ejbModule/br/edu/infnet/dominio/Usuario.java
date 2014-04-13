b89221d8a8d08ee86bf841c5c5fa9d183fed718c
Vitor <vitoremanuelcruzvieira@gmail.com>
2014-04-13 20:41:48 -0300

    Criada a classe Usuário

---
+++ avaliacao-infnet-ejb/ejbModule/br/edu/infnet/dominio/Usuario.java
@@ -0,0 +1,55 @@
+package br.edu.infnet.dominio;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usuario")
+public class Usuario {
+	@Id
+	@Column(name = "idusuario")
+	private Integer id;
+	@Column(name = "nome")
+	private String nome;
+	@Column(name = "login")
+	private String login;
+	@Column(name = "senha")
+	private String senha;
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
