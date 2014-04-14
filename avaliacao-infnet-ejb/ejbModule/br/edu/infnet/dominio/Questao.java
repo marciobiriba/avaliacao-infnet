f5217cbdbdfa7ea5e9d0679c838b5b2575f67427
Vitor <vitoremanuelcruzvieira@gmail.com>
2014-04-13 21:31:21 -0300

    Preenchendo a classe Questão

--- avaliacao-infnet-ejb/ejbModule/br/edu/infnet/dominio/Questao.java
+++ avaliacao-infnet-ejb/ejbModule/br/edu/infnet/dominio/Questao.java
@@ -1,5 +1,34 @@
 package br.edu.infnet.dominio;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="questao")
 public class Questao {
-	
+
+	@Id
+	@Column(name = "idquestao")
+	private Integer id;
+	@Column(name = "descricao")
+	private String descricao;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
 }
