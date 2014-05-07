69ff355c32bbcfede726ee97b5b760dbcd57acc7
Marcio Biriba <marcio.biriba@gmail.com>
2014-05-07 16:29:27 -0300

    Modificada AbstractDAO.

--- avaliacao-infnet-ejb/ejbModule/br/edu/infnet/dao/AbstractDAO.java
+++ avaliacao-infnet-ejb/ejbModule/br/edu/infnet/dao/AbstractDAO.java
@@ -1,23 +1,51 @@
 package br.edu.infnet.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
 
 public abstract class AbstractDAO<T> {
-	@PersistenceContext(unitName="avaliacao-infnet-ejbPU")
+private Class<T> entidade;
+	
+	@PersistenceContext(unitName = "avaliacao-infnet-ejbPU")
 	private EntityManager em;
-	private Class<T> entityClass;
-	public AbstractDAO(Class<T> entityClass){
-		this.setEntityClass(entityClass);
+	
+	protected EntityManager getEntityManager(){
+		return this.em;
 	}
-	public Class<T> getEntityClass() {
-		return entityClass;
-	}
-	public void setEntityClass(Class<T> entityClass) {
-		this.entityClass = entityClass;
-	}
-	public void cadastrar(T entity){
-        em.persist(entity);
+	
+	public AbstractDAO(Class<T> entidade) {
+        this.entidade = entidade;
     }
+	
+	public void cadastrar(T entidade){
+		getEntityManager().persist(entidade);
+	}
+	
+	public void alterar(T entidade){
+		getEntityManager().merge(entidade);
+	}
+	
+	public void excluir(T entidade){
+		getEntityManager().remove(getEntityManager().merge(entidade));
+	}
+	
+	public T obterPorId(Integer id){
+		return getEntityManager().find(entidade, id);
+	}
+	
+	public List<T> listar(){
+        CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entidade));
+        return getEntityManager().createQuery(cq).getResultList();
+    }
+	/*public List consulta(){
+    	return em.createNativeQuery("Select p from Pedido where p.idProduto = :IDPROD")
+    		.setParameter("IDPROD", "1")
+    		.setMaxResults(50)
+    		.getResultList();
+    }*/
 
 }
